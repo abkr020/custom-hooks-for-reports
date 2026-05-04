a4848d52d195a4a5199196f77923445bf45a71c6
abkr020 <abkr020@gmail.com>
2026-05-04 23:58:36 +0530

    it worked

--- src/reports/Report2.jsx
+++ src/reports/Report2.jsx
@@ -1,12 +1,30 @@
-import React from 'react'
+import React from "react";
+import useReportFetchWithFilter from "../hooks/useReportFetchWithFilter";
 
 const Report2 = () => {
+  const url = "https://jsonplaceholder.typicode.com/users";
+
+  const { data, loading, error, InpurLimitComponent } =
+    useReportFetchWithFilter(url);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error}</p>;
+
   return (
     <div>
-      Report2
+      <h2>Report 2 (Users)</h2>
 
+      <InpurLimitComponent />
+
+      {data?.map((user) => (
+        <div key={user.id} style={{ marginBottom: "15px" }}>
+          <h4>{user.name}</h4>
+          {/* <p>{user.email}</p> */}
+          {/* <p>{user.company?.name}</p> */}
+        </div>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default Report2
+export default Report2;
