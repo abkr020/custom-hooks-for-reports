77d69b55edb29ed9011b3fb2558fdeedc83e84bd
abkr020 <abkr020@gmail.com>
2026-05-05 00:15:52 +0530

    workign same common logic for both

--- src/reports/Report2.jsx
+++ src/reports/Report2.jsx
@@ -2,29 +2,36 @@ import React from "react";
 import useReportFetchWithFilter from "../hooks/useReportFetchWithFilter";
 
 const Report2 = () => {
-  const url = "https://jsonplaceholder.typicode.com/users";
+    const url = "https://jsonplaceholder.typicode.com/users";
 
-  const { data, loading, error, InpurLimitComponent } =
-    useReportFetchWithFilter(url);
+    const { data, loading, error, InpurLimitComponent } =
+        useReportFetchWithFilter(url);
 
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error: {error}</p>;
 
-  return (
-    <div>
-      <h2>Report 2 (Users)</h2>
 
-      <InpurLimitComponent />
+    return (
+        <div>
+            <h2>Report 2 (Users)</h2>
 
-      {data?.map((user) => (
-        <div key={user.id} style={{ marginBottom: "15px" }}>
-          <h4>{user.name}</h4>
-          {/* <p>{user.email}</p> */}
-          {/* <p>{user.company?.name}</p> */}
+            {InpurLimitComponent()}
+            {RenderSearchInputComponent()}
+
+            {/* loading indicator (non-blocking) */}
+            {loading && <p style={{ color: "blue" }}>Loading...</p>}
+
+            {/* error */}
+            {error && <p style={{ color: "red" }}>{error}</p>}
+
+
+            {data?.map((user) => (
+                <div key={user.id} style={{ marginBottom: "15px" }}>
+                    <h4>{user.name}</h4>
+                    {/* <p>{user.email}</p> */}
+                    {/* <p>{user.company?.name}</p> */}
+                </div>
+            ))}
         </div>
-      ))}
-    </div>
-  );
+    );
 };
 
 export default Report2;
